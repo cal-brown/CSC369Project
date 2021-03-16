00c817caae77d9c60095680fa545feb60cad2984
unknown <calvinlongbrown@gmail.com>
2021-03-16 14:05:42 -0700

    configured spark/changed scala version

---
+++ src/main/scala/PopularityPredictor.scala
@@ -0,0 +1,20 @@
+import org.apache.spark.{ SparkConf, SparkContext }
+import org.apache.spark.rdd._
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
+class PopularityPredictor {
+
+}
+
+object PopularityPredictor {
+  def main(args: Array[String]):Unit = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    val conf = new SparkConf().setAppName("NameOfApp").setMaster("local[4]")
+    val sc = new SparkContext(conf)
+
+    sc.textFile("data/data.csv").take(2).foreach(println)
+  }
+}
