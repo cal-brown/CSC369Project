12bb6ff4fd2b98e3ae13a1d65c7980e73092ae30
Notchanator <55802661+Notchanator@users.noreply.github.com>
2021-03-17 21:08:07 -0700

    Updating my code
    Now they read tsv

--- src/main/scala/AverageSongTime.scala
+++ src/main/scala/AverageSongTime.scala
@@ -8,16 +8,17 @@ object AverageSongTime {
     Logger.getLogger("akka").setLevel(Level.OFF)
     val conf = new SparkConf().setAppName("Artist by average song length").setMaster("local[4]")
     val sc = new SparkContext(conf)
-    println("")
-    sc.textFile("data.csv").map(x=>(x.split(",")(1), x.split(",")(3))).
+    val data = sc.textFile("data.tsv").map(x=>(x.split("\t")(1), x.split("\t")(3))).
       mapPartitionsWithIndex((index, it) => if (index == 0) it.drop(1) else it, preservesPartitioning = true).
-      filter(_._2.matches("[0-9]+")).
+      map({case(x,y) => (x.split(", ").toList, y)}).map({case(x,y) => x.map(z=>(z,y))}).flatMap(l=>l).
       map({case(x,y) => (x.replaceAll("""[\['\]?"]""", ""), y.toInt)}).groupByKey().
       map({case(x,y) => (x, y.aggregate((0,0))((x,y) => (x._1 + y, x._2 +1), (x,y) => (x._1 + y._1, x._2 + y._2)))}).
       map({case(x,y) => (x, (y._1 * 1.0 / y._2) / 1000)}).
-      sortBy(_._2).map({case(x,y) => s"$x: ${y}seconds"}). //shortest songwriters
-      //sortBy(-_._2).map({case(x,y) => s"$x: ${y}seconds or ${(y/60).toInt}minutes ${(y%60).toInt}seconds"}). //longest songwriters
-      take(10).foreach(println(_))
+      //sortBy(_._2).map({case(x,y) => s"$x: ${y}seconds"}). //shortest songwriters
+      sortBy(-_._2).map({case(x,y) => s"$x: ${y}seconds or ${(y/60).toInt}minutes ${(y%60).toInt}seconds"}). //longest songwriters
+      collect()
+    println("") //So that output spacing is nice
+    data.take(10).foreach(println(_))
   }
  """val spark = SparkSession.builder.appName("Simple Application").master("local[4]").getOrCreate()"""
  """val data_csv = spark.read.option("header", true)     .option("inferSchema", "true")      .csv("data/data.csv")"""
